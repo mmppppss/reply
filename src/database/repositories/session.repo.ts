e97c89edb7f38321f3b2c36a973b7209f304f9a1
mmppppss <ppozosoliz@gmail.com>
2026-02-11 21:28:54 -0400

    add jsdoc

--- src/database/repositories/session.repo.ts
+++ src/database/repositories/session.repo.ts
@@ -1,30 +1,52 @@
+// @ts-check
 import { eq } from "drizzle-orm";
 import { db } from "../index";
 import { sessions } from "../schema/sessions.schema";
 
 
-export class Session{
-    async findAll(){
-        return db.select().from(sessions);
-    }
-    async create(data:any){
-        const result = await db
-            .insert(sessions)
-            .values(data);
-        return result;
-    }
-    async deleteById(id:number){       
-        const result = await db
-            .delete(sessions)
-            .where(eq(sessions.id, id))
-        return result;
-    }
-    async update(id:number, data: any){
-        const result =await db
-            .update(sessions)
-            .set(data)
-            .where(eq(sessions.id, id))
-        return result[0].affectedRows > 0;
+export class Session {
+	/**
+	 * Find all sessions
+	 *
+	 * @returns array sesions
+	 */
+	async findAll() {
+		return db.select().from(sessions);
 	}
-    
+
+	/**
+	 * Create 
+	 *
+	 */
+	async create(data: any) {
+		const result = await db
+			.insert(sessions)
+			.values(data);
+		return result;
+	}
+	/**
+	 * deleteById.
+	 *
+	 * @param {number} id
+	 */
+	async deleteById(id: number) {
+		const result = await db
+			.delete(sessions)
+			.where(eq(sessions.id, id))
+		return result;
+	}
+	/**
+	 * update.
+	 *
+	 * @param {number} id
+	 * @param {any} data
+	 */
+	async update(id: number, data: any) {
+		const result = await db
+			.update(sessions)
+			.set(data)
+			.where(eq(sessions.id, id))
+		return result[0].affectedRows > 0;
+	}
+
 }
