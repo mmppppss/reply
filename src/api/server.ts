170be87426e7d94b80a3642eb271cc93d54da403
mmppppss <ppozosoliz@gmail.com>
2026-03-29 14:02:59 -0400

    feat: ipv6 support

--- src/api/server.ts
+++ src/api/server.ts
@@ -2,9 +2,10 @@
 import app from "./app";
 
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.API_HOST_v6 || "::";
 
 export default async function startApi() {
-	app.listen(PORT, () => {
-		console.log(`Server running on http://localhost:${PORT}`);
+	app.listen(Number(PORT), HOST, () => {
+		console.log(`Server running on http://${HOST}:${PORT}`);
 	});
 }
